146c2cef092d0009d8c2fbc3ccff4037bd428651
MaggieOsorio <margarita.escu.1988@gmail.com>
2016-02-06 12:45:31 -0600

    Exito en controlador central
    CONFIG: se modifica el prefijo
    ROUTERS: se agrega el controlador
    UADMIN: se llaman las vistas
    LOGIN_CONTROLLER: se terminan las clases y la función
    MODELO: se agregan metodos.

--- Session/application/core/LOGING_Controller.php
+++ Session/application/core/LOGING_Controller.php
@@ -8,44 +8,71 @@
 	class LOGING_Controller extends CI_Controller
 	{
 		
-		function __construct()
+		 public function __construct()
 		{
 			parent::__construct();
 				//cargar librerias, helpers 
 				//clase Session inicializada en el controlador, para poder hacer uso de ella
-				$this->load->library('sesion');
+				$this->load->library('session');
 				//carga el archivo de configuracion
-				$this->config->load->('');
+				//$this->config->load->('');
 				//carga la base de datos
 				$this->load->database();
 				//caraga el helper URL 
-				$this->load->helper->('url');
-		}
+				//$this->load->helper->('url');
+				$this->load->model('modelo_session');
+		}		
+	}//login controller
 
-		/**
+	/**
 		* clase par evitar ciclo
 		**/
 		class Loging extends LOGING_Controller
 		{
 			
-			function __construct()
+			 public function __construct()
 			{
 				parent::__construct();
 			}
 
-	
-			//Preguntamos si la sesion esta activa()
-			if($this->session->userdata('usuario') != NULL){
-				//Va a preguntar que tipo de usuario es o sesion es
-		        $usuario = $this->session->userdata();
+			
 
-			}else{//Si la sesion no esta activa
-				//Te manda al login
-			}
+			public function valida_sesion(){
 
+				$datos = array('user');
+				$this->session->unset_userdata($datos);
+				
 
-		}
-	}
+				$user= $this->uri->segment(2);
+				$pass= $this->uri->segment(3);
+
+				$arr = array('user'=> '$user',
+							'pass'=> '$pass');
+				if($this->session->has_userdata('user')){
+					$rol = $this->modelo_session->rol($arr);
+					echo $rol;
+					if($rol == "admin"){
+						return "admin";
+					}else{
+						return "user";
+					}
+
+				}else{
+					//Su el usuario esta en la base, se inicializa sesión
+					if($this->modelo_session->verificaUser($user)){
+						$this->session->set_userdata($arr);
+						echo $this->session->has_userdata('user') ;
+					}//if
+					else{
+						echo "Usuario incorrecto";
+					}//else
+				}//else
+				return "";
+
+			}//valida session
+
+			
+			}//login controller
 
 
 ?>
--- Session/application/models/Modelo_session.php
+++ Session/application/models/Modelo_session.php
@@ -9,14 +9,26 @@ class Modelo_session extends CI_Model{
 	
 	public function verificaUser($user){
 		$this->db->select('*');
-		$this->db->from('user');
-		$this->db->where('user',$user);
+		$this->db->from('users');
+		$this->db->where('Usuario',$user);
 		$query = $this->db->get();
 		$result = $query->result_array();
 		if(empty($result))
 		    return FALSE;
 		else
 		    return TRUE;
+	}
+
+	public function rol($user){
+		$this->db->select('Rol');
+		$this->db->from('users');
+		$this->db->where('Usuario','user');
+		$query = $this->db->get();
+		$result = $query->result_array();
+		if(empty($result))
+		    return $result;
+		else
+		    return "";
 	}
 
 
